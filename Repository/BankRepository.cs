e12feb6b56be2a45af67cf7689b780c64c5958ab
Anuradha-SNAD <anuradha.avaduthwar@snaddevelopers.com>
2026-07-03 19:40:13 +0530

    Add files via upload

---
+++ Repository/BankRepository.cs
@@ -0,0 +1,147 @@
+﻿using BankManagementSystem.Data;
+using BankManagementSystem.DTOs;
+using BankManagementSystem.Exceptions;
+using BankManagementSystem.Model;
+
+namespace BankManagementSystem.Repository
+{
+    public class BankRepository : IBankRepository
+    {
+        private AppDbContext context;
+
+        public BankRepository(AppDbContext context)
+        {
+            this.context = context;
+        }
+        public void Add(BankAccountRequestDTO dto)
+        {
+            BankAccount b = new BankAccount()
+            {
+                HolderName = dto.HolderName,
+                AccountNumber = dto.AccountNumber,
+                AccountType = dto.AccountType,
+                AccountBalance = dto.AccountBalance,
+                BranchName = dto.BranchName,
+                IFSCCode = dto.IFSCCode,
+                CreatedDate = dto.CreatedDate,
+            };
+            context.bank.Add(b);
+            context.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            var bank = context.bank.FirstOrDefault(e=>e.AccountId == id);
+           if(bank != null)
+            {
+                context.bank.Remove(bank);
+                context.SaveChanges();
+            }
+
+        }
+
+        public void Deposit(DepositRequestDTO dto)
+        {
+           var account = context.bank.FirstOrDefault(x => x.AccountNumber == dto.AccountNumber);
+
+            if (account == null)
+            {
+                return;
+            }
+
+            account.AccountBalance += dto.AccountBalance;
+
+            context.SaveChanges();
+
+        }
+
+        public void Withdraw(WithdrawRequestDTO dto)
+        {
+            var b = context.bank.FirstOrDefault(x => x.AccountNumber == dto.AccountNumber);
+            b.AccountBalance -= dto.AccountBalance;
+            context.SaveChanges();
+        }
+
+        public void Transfer(TransferRequestDTO dto)
+        {
+            var sender = context.bank.FirstOrDefault(x => x.AccountNumber == dto.FromAccount);
+            var receiver = context.bank.FirstOrDefault(x => x.AccountNumber == dto.ToAccount);
+
+            if(sender == null || receiver == null)
+            {
+                return;
+            }
+            sender.AccountBalance -= dto.Amount;
+            receiver.AccountBalance += dto.Amount;
+            context.SaveChanges();
+        }
+
+        public List<BankAccountResponseDTO> GetAll()
+        {
+            return context.bank.Select(b => new BankAccountResponseDTO()
+            {
+                HolderName = b.HolderName,
+                AccountNumber = b.AccountNumber,
+                AccountBalance = b.AccountBalance
+            }).ToList();
+        }
+
+        public BankAccountResponseDTO GetById(int id)
+        {
+            var bank = context.bank.FirstOrDefault(b => b.AccountId == id);
+            if(bank == null)
+            {
+                return null;
+            }
+            return new BankAccountResponseDTO()
+            {
+                HolderName = bank.HolderName,
+                AccountNumber = bank.AccountNumber,
+                AccountBalance = bank.AccountBalance
+
+            };
+        }
+
+        public BankAccountResponseDTO SearchAccountNumber(string accountNumber)
+        {
+            return context.bank
+                .Where(e => e.AccountNumber == accountNumber)
+                .Select(b => new BankAccountResponseDTO
+                {
+                    HolderName = b.HolderName,
+                    AccountNumber = b.AccountNumber,
+                    AccountBalance = b.AccountBalance
+                })
+                .FirstOrDefault();
+        }
+
+
+        public List<BankAccountResponseDTO> SearchByName(string name)
+        {
+            return context.bank.Where(e => e.HolderName.ToLower().Contains(name.ToLower())).Select(b => new BankAccountResponseDTO
+            {
+                HolderName = b.HolderName,
+                AccountNumber = b.AccountNumber,
+                AccountBalance= b.AccountBalance
+
+            }).ToList();
+           
+        }
+
+        public void Update(int id, BankAccountRequestDTO dto)
+        {
+            var bank = context.bank.FirstOrDefault(b => b.AccountId == id);
+            if(bank != null)
+            {
+                bank.HolderName = dto.HolderName;
+                bank.AccountNumber = dto.AccountNumber;
+                bank.AccountType = dto.AccountType;
+                bank.AccountBalance = dto.AccountBalance;
+                bank.BranchName = dto.BranchName;
+                bank.IFSCCode = dto.IFSCCode;
+                context.SaveChanges();
+            }
+
+        }
+    }
+}
---
+++ Repository/IBankRepository.cs
@@ -0,0 +1,25 @@
+﻿using BankManagementSystem.DTOs;
+
+namespace BankManagementSystem.Repository
+{
+    public interface IBankRepository
+    {
+        void Add(BankAccountRequestDTO dto);
+
+        List<BankAccountResponseDTO> GetAll();
+
+        BankAccountResponseDTO GetById(int id);
+
+        void Update(int id, BankAccountRequestDTO dto);
+        void Delete(int id);
+
+        List<BankAccountResponseDTO> SearchByName(string name);
+
+        BankAccountResponseDTO SearchAccountNumber(string AccountNumber);
+
+        void Deposit(DepositRequestDTO dto);
+        void Withdraw(WithdrawRequestDTO dto);
+
+        void Transfer(TransferRequestDTO dto);
+    }
+}
